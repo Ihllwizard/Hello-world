ee82343acf0ad8859480398708daf60fbdd5c47a
ihllwizard <temitope.idowu27@gmail.com>
2020-01-28 09:05:30 +0100

    work project

---
+++ Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace jacky
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Guid imagename=Guid.NewGuid();
+            Console.WriteLine(imagename);
+        }
+    }
+}
